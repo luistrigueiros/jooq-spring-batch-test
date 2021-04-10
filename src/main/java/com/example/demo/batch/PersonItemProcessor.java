e2a0b9e8c13505fe278405a25c214b0f90a94475
luist <luistrigueiros@gmail.com>
2021-04-10 17:08:16 +0100

    Enabled lombok

--- src/main/java/com/example/demo/batch/PersonItemProcessor.java
+++ src/main/java/com/example/demo/batch/PersonItemProcessor.java
@@ -14,7 +14,10 @@ public class PersonItemProcessor implements ItemProcessor<Person, Person> {
 		final String firstName = person.getFirstName().toUpperCase();
 		final String lastName = person.getLastName().toUpperCase();
 
-		final Person transformedPerson = new Person(firstName, lastName);
+		final Person transformedPerson = new Person() {{
+			setFirstName(firstName);
+			setLastName(lastName);
+		}};
 
 		log.info("Converting (" + person + ") into (" + transformedPerson + ")");
 
