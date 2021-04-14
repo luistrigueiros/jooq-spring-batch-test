fb252fb1047c15d8e2530ff375e167008f65bda6
luist <luistrigueiros@gmail.com>
2021-04-14 22:45:50 +0100

    Fix test error while translate exception

--- src/main/java/com/example/demo/config/SpringExceptionTranslationExecuteListener.java
+++ src/main/java/com/example/demo/config/SpringExceptionTranslationExecuteListener.java
@@ -3,26 +3,29 @@ package com.example.demo.config;
 import org.jooq.ExecuteContext;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DefaultExecuteListener;
-
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.UncategorizedSQLException;
 import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLExceptionTranslator;
 import org.springframework.jdbc.support.SQLStateSQLExceptionTranslator;
 
+import java.util.Objects;
+
 public class SpringExceptionTranslationExecuteListener
         extends DefaultExecuteListener {
 
-  @Override
-  public void exception(ExecuteContext ctx) {
-    SQLDialect             dialect    = ctx.configuration().dialect();
-    SQLExceptionTranslator translator = getSqlExceptionTranslator(dialect);
-    DataAccessException jOOQ = translator.translate("jOOQ", ctx.sql(), ctx.sqlException());
-    ctx.exception(jOOQ);
-  }
+    @Override
+    public void exception(ExecuteContext ctx) {
+        SQLDialect dialect = ctx.configuration().dialect();
+        SQLExceptionTranslator translator = getSqlExceptionTranslator(dialect);
+        DataAccessException dataAccessException = translator.translate("Data access using JOOQ", ctx.sql(), ctx.sqlException());
+        DataAccessException translation = Objects.requireNonNullElseGet(dataAccessException, () -> new UncategorizedSQLException("translation of exception", ctx.sql(), ctx.sqlException()));
+        ctx.exception(translation);
+    }
 
-  private SQLExceptionTranslator getSqlExceptionTranslator(SQLDialect dialect) {
-    return (dialect != null)
-            ? new SQLErrorCodeSQLExceptionTranslator(dialect.name())
-            : new SQLStateSQLExceptionTranslator();
-  }
+    private SQLExceptionTranslator getSqlExceptionTranslator(SQLDialect dialect) {
+        return (dialect != null)
+                ? new SQLErrorCodeSQLExceptionTranslator(dialect.name())
+                : new SQLStateSQLExceptionTranslator();
+    }
 }
